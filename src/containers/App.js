2564f62f65b6cb041add570707649c768a41e54b
greedy17 <61763082+greedy17@users.noreply.github.com>
2020-12-09 18:08:37 -0500

    created file structure for app

---
+++ src/containers/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cardNumber: 0,
+      collection: []
+    }
+
+  }
+
+  componentDidMount() {
+    axios.get('put collection endpoint here')
+    .then(res => {
+      const collection = res.data;
+      this.setState({collection: collection})
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default App;
