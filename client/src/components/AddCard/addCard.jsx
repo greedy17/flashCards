fb1f6aac65322b4412e4b171298252516a938d5a
MichelleRoubikLee <michelleroubiklee@gmail.com>
2020-12-15 11:46:08 -0600

    add card submit button takes values from form

--- client/src/components/AddCard/addCard.jsx
+++ client/src/components/AddCard/addCard.jsx
@@ -4,19 +4,25 @@ import './addCard.css'
   class addCard extends React.Component{
     constructor(props){
       super(props);
-      this.state = {value: ''};
+      this.state = {
+        cardFront: "",
+        cardBack: ""
+      };
 
-      this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
 
     handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
       event.preventDefault();
+      const inputs = event.target.getElementsByTagName('input');
+      console.log(inputs.cardFront.value);
+      console.log(inputs.cardBack.value);
+
+      this.setState({
+        cardFront: inputs.cardFront.value,
+        cardBack: inputs.cardBack.value
+      })      
     }
     
   
@@ -28,18 +34,29 @@ import './addCard.css'
         </div>
   
         <div className="modal fade" id="addCardModal" tabindex="-1" role="dialog" aria-labelledby="addCardModalTitle" aria-hidden="true">
+        
           <div className="modal-dialog modal-dialog-centered" role="document">
             <div className="modal-content">
               <div className="modal-header">
-                <h5 className="modal-title" id="addCardModalTitle">Modal title</h5>
+                <div className="modal-title" id="addCardModalTitle">Modal title</div>
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
               <form onSubmit={this.handleSubmit}>
                 <div className="modal-body">
-                  <label>Front: </label><input class="form-control" id="cardFront" type="text" value={this.state.input} onChange={this.handleChange}></input>
-                  <label>Back: </label><input class="form-control" id="cardBack" type="text" value={this.state.input} onChange={this.handleChange}></input>
+                  <label>Front: </label>
+                  <input 
+                    name="cardFront"
+                    class="form-control"  
+                    type="text" >
+                  </input>
+                  <label>Back: </label>
+                  <input 
+                    name="cardBack"
+                    class="form-control"
+                    type="text" >
+                  </input>
                 </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
